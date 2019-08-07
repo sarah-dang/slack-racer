120a1497d1958b9112ddcf92fc81c2a1daf950bf
James Lau <jamesylau@gmail.com>
2019-08-07 13:42:35 -0700

    Implemented ngrok.

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ import slack
 token = 'xoxb-707886202387-721598349094-Z2VDHIYjrumQbqmUUNlvMu0j'
 client = slack.WebClient(token=token)
 
+'''
 response = client.chat_postMessage(
     username="Slack Racer Referee",
-    icon_emoji=":bearded_person:",
+    icon_emoji=":checkered_flag:",
     channel="general",
     text="Sahand is the BEST.",
 )
+'''
+
+import asyncio
+
+loop = asyncio.get_event_loop()
+
+client = slack.WebClient(
+    token=token,
+    run_async=True
+    )
+
+response = loop.run_until_complete(client.chat_postMessage(
+        channel='#random',
+        text="Hello world!"
+        )
+        )
+        
+assert response["ok"]
+assert response["message"]["text"] == "Hello world!"
